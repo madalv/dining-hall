6e77dd6185383a37e161154d2c87fcd75d6bbe7e
madalv <magalvlada.s@gmail.com>
2022-09-13 15:53:16 +0300

    configured Dockerfile & docker-compose.yml

---
+++ src/main/kotlin/com/madalv/GlobalConfig.kt
@@ -0,0 +1,13 @@
+package com.madalv
+
+object cfg {
+    val kitchen: String = "kitchen"
+    val timeUnit: Int = 1000
+    val nrTables: Int = 10
+    val nrWaiters: Int = 4
+    val maxItemsPerOrder: Int = 10
+    val orderIdMax = 500
+
+    val waitTimeCoeff = 1.3
+}
+
